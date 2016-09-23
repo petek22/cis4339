14f890df3ef2546826cd08949a0f44d290c3377b
Pete Kamenszky <petek_22@outlook.com>
2016-09-23 13:13:29 -0500

    sprint 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :docks
   resources :dock_workers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+get 'welcome/home', to: 'welcome#home'
+  get 'welcome/about', to: 'welcome#about'
 end
